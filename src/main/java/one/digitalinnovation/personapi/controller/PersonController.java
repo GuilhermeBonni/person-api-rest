8747ba05f84bd46359e25e4d5cca0a96d734d09d
Guilherme Bonni <bonni.guilherme@gmail.com>
2022-11-22 10:19:14 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation.personapi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class PersonController {
+    
+    @GetMapping
+    public String getBook() {
+    	return "Página de Guilherme Bonni";
+    }
+}
